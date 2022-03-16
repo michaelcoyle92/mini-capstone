66ad6291d4543122d9865e5697a3631af7a04b4a
MICHAEL COYLE <MACCOYLE@GMAIL.com>
2022-03-16 12:43:39 -0400

    updated schema with integers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,12 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "example.com", "mini-capstone.peterxjang.com"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
---
+++ db/migrate/20220316163431_add_inventoryto_products.rb
@@ -0,0 +1,6 @@
+class AddInventorytoProducts < ActiveRecord::Migration[7.0]
+  def change
+    add_column :products, :inventory, :integer
+  
+  end
+end
