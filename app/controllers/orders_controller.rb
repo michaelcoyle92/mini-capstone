959c9dd6424f50199ed5138afe430096826aa0b4
MICHAEL COYLE <MACCOYLE@GMAIL.com>
2022-03-22 16:01:09 -0400

    update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,13 @@ class OrdersController < ApplicationController
   def show
     order = Order.find_by(id: params[:id])
     render json: order.as_json
+    @order = Order.find_by(id: params[:id])
+    if @order.user_id == current_user.id
+      render template: "orders/show"
+    else
+      render json: {}, status: :unauthorized
+    end
+  end
   end
 
   def index
@@ -12,16 +19,24 @@ class OrdersController < ApplicationController
   
   
   def create
+    
+    
+    product = Product.find_by(id: params[:product_id])    
+    calculated_subtotal = params[:quantity].to_i * product.price
+    calculated_tax = calculated_subtotal * 0.09
+
     order = Order.new(
+      user_id: params[:user_id],
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
       subtotal: params[:subtotal],
       tax: params[:tax],
-      total: params[:total]
-
+      total: params[:total],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_subtotal + calculated_tax,
     )
     order.save
     render json: order.as_json
-  end
 end
