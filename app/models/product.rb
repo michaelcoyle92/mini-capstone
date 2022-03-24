565b28258b3ecc3c244db6731c9517ec07cc4556
MICHAEL COYLE <MACCOYLE@GMAIL.com>
2022-03-24 14:20:09 -0400

    added associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,11 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
   validates :description, presence: true
   validates :description, length: {in: 10...500}
+  
+  belongs_to :supplier
+  has_many :images
+  has_many :category_products
+  has_many :categories, through: :category_products
 
   def supplier
     Supplier.find_by(id: supplier_id)
